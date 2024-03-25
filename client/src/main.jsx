5a547fcc000e6da8bacec4a4d92714a8c88c3c5f
EmaVillalba <emasv23@gmail.com>
2024-03-25 11:12:32 -0300

    Config finish

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import store from './store.js'
+import store from './redux/store.js'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
